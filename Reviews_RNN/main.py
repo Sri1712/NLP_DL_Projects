762f45a18ab5e637a51de074c364ba1e24d47a3b
Srilathaa Vasu <58694589+Sri1712@users.noreply.github.com>
2026-05-24 11:22:23 +0000

    Changes in main.py file

--- Reviews_RNN/main.py
+++ Reviews_RNN/main.py
@@ -20,34 +20,29 @@ def decode_review(encoded_review):
 # Function to preprocess user input review
 def preprocess_review(review):
     words = review.lower().split() # Convert review to lowercase and split into words
-    encoded_review = [word_index.get(word, 0) for word in words] # Convert words to their corresponding indices based on the word index
-    padded_review = sequence.pad_sequences([encoded_review], maxlen=500) # Pad the sequence to ensure it has the same length as the training data
+    encoded_review = [word_index.get(word, 2) +3 for word in words] # Convert words to their corresponding indices based on the word index
+    padded_review = sequence.pad_sequences([encoded_review], maxlen=500, padding = 'pre', truncating= 'pre') # Pad the sequence to ensure it has the same length as the training data
     return padded_review
-
-# Step 3: Predict Sentiment
-# Predict the sentiment of a user input review
-
-def predict_sentiment(review):
-    processed_review = preprocess_review(review) # Preprocess the review
-    prediction = model.predict(processed_review) # Make a prediction using the model
-    sentiment = 'Positive' if prediction[0][0] >= 0.5 else 'Negative' # Interpret the prediction
-    confidence = prediction[0][0] if prediction[0][0] >= 0.5 else 1 - prediction[0][0]
-    return sentiment, confidence
-
 
 # Streamlit app
 import streamlit as st
 st.title("IMDB Movie Review Sentiment Analysis")
+st.write('Enter a movie review to classify it as positive or negative.')
 
 # Input field for user review
 user_review = st.text_area("Enter a movie review:")
 
 # Button to trigger sentiment prediction
 if st.button("Predict Sentiment"):
-    if user_review.strip() == "":
-        st.warning("Please enter a movie review.")
-    else:
-        preprocess_input = preprocess_review(user_review)
-        sentiment, confidence = predict_sentiment(user_review)
-        st.success(f"Predicted Sentiment: {sentiment}")
-        st.info(f"Confidence Score: {confidence:.2f}")
+
+    preprocessed_input=preprocess_review(user_review)
+
+    ## Make prediction
+    prediction=model.predict(preprocessed_input)
+    sentiment='Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    # Display the result
+    st.write(f'Sentiment: {sentiment}')
+    st.write(f'Prediction Score: {prediction[0][0]}')
+else:
+    st.write('Please enter a movie review.')
